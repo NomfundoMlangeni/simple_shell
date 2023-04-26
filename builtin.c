3678e4ed8e653229cc7988eb776f30b5f608c690
Nomfundo Mlangeni <122948300+NomfundoMlangeni@users.noreply.github.com>
2023-04-26 18:22:40 -0400

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,18 +1,20 @@
 #include "shell.h"
+
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments.To be used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
  * Return: exits with a given exit status
  * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
-	int checkexit;
+	int exitcheck;
 
 	if (info->argv[1]) /* If there is an exit arguement */
 	{
-		checkexit = _erratoi(info->argv[1]);
-		if (checkexit == -1)
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
@@ -29,13 +31,14 @@ int _myexit(info_t *info)
 
 /**
  * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments.To be used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
  * Return: Always 0
  */
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
-	int chadir_ret;
+	int chdir_ret;
 
 	s = getcwd(buffer, 1024);
 	if (!s)
@@ -44,10 +47,10 @@ int _mycd(info_t *info)
 	{
 		dir = _getenv(info, "HOME=");
 		if (!dir)
-			chadir_ret = /* TODO: what should this be? */
+			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
-			chadir_ret = chdir(dir);
+			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
@@ -58,12 +61,12 @@ int _mycd(info_t *info)
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chadir_ret = /* TODO: what should this be? */
+		chdir_ret = /* TODO: what should this be? */
 			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
-		chadir_ret = chdir(info->argv[1]);
-	if (chadir_ret == -1)
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
@@ -78,7 +81,8 @@ int _mycd(info_t *info)
 
 /**
  * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments.To be used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
  * Return: Always 0
  */
 int _myhelp(info_t *info)
@@ -91,4 +95,3 @@ int _myhelp(info_t *info)
 		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
-
